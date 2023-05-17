8c5eb2722a8f8ea45a971069c532db00f784deee
Janie Liu <59671956+JanieLiu642@users.noreply.github.com>
2023-05-17 01:14:44 -0400

    Update js.js

--- js.js
+++ js.js
@@ -5,6 +5,27 @@ $.getJSON("data.json", function(trip) {
     const endDate = addDay(new Date(trip[0]['start']), duration - 1);
     const tip = trip[0]['tip'];
 
+    $('trip').text(`${name}'s Itinerary to ${tripTitle}`);
+    $('timeline').text(`${obtainDate(new Date(trip[0]['start']))} - ${obtainDate(endDate)}`);
+    $('tip > p').text(`${tip}`);
+
+    let main = '';
+    for (let i = 0; i < duration; i++) {
+        const currentDate = addDay(new Date(trip[0]['start']), i);
+        const dayTitle = trip.find(rec => rec['day'] === i + 1)['title'];
+        const html = `<day class="row-align">
+                    <date class="row-align column-align">
+                        <h1>Day ${i + 1}</h1>
+                        <p>${obtainWeekday(currentDate)}</p>
+                        <p>${obtainDate(currentDate)}</p>
+                    </date>
+                    <section>
+                        <h2>${dayTitle}</h2>
+                        <ul>` + obtainDesc(i + 1) + `</ul></section></day>`;
+        main += html;
+    }
+    $('main').empty().append(main);
+
     function obtainDate(d) {
         const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
         return`${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
@@ -29,29 +50,4 @@ $.getJSON("data.json", function(trip) {
         }
         return html;
     }
-
-    $('trip').text(`${name}'s Itinerary to ${tripTitle}`);
-    $('timeline').text(`${obtainDate(new Date(trip[0]['start']))} - ${obtainDate(endDate)}`);
-    $('tip > p').text(`${tip}`);
-
-    $('main').empty();
-    let main = '';
-    for (let i = 0; i < duration; i++) {
-        const currentDate = addDay(new Date(trip[0]['start']), i);
-        const dayTitle = trip.find(rec => rec['day'] === i + 1)['title'];
-        let dayBlock = '';
-        dayBlock += `<day class="row-align">
-                    <date class="row-align column-align">
-                        <h1>Day ${i + 1}</h1>
-                        <p>${obtainWeekday(currentDate)}</p>
-                        <p>${obtainDate(currentDate)}</p>
-                    </date>
-                    <section>
-                        <h2>${dayTitle}</h2>
-                        <ul>`
-        dayBlock += obtainDesc(i + 1);
-        dayBlock += `</ul></section></day>`;
-        main += dayBlock;
-    }
-    $('main').append(main);
 });
